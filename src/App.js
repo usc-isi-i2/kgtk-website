d829166be974cf14dd01f2be0f78b489f7f2aad4
g1eb <mail@g1eb.com>
2022-11-08 11:30:02 -0800

    Update the main App component to include a base css theme

--- src/App.js
+++ src/App.js
@@ -1,21 +1,105 @@
-function App() {
+import React from 'react'
+
+import CssBaseline from '@mui/material/CssBaseline'
+
+import {
+  createTheme,
+  ThemeProvider,
+  responsiveFontSizes,
+} from '@mui/material/styles'
+
+
+let theme = createTheme({
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        html: {
+          WebkitFontSmoothing: 'auto',
+        },
+        body: {
+          background: '#fefefe',
+          color: '#333',
+        },
+      },
+    },
+    MuiAppBar: {
+      colorPrimary: {
+        backgroundColor: '#fefefe',
+        color: '#333',
+      },
+    },
+    MuiFormLabel: {
+      root: {
+        '&.Mui-focused': {
+          color: '#111',
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      root: {
+        borderRadius: 0,
+        '&:hover': {
+          cursor: 'pointer',
+        },
+        '& fieldset': {
+          borderColor: '#333',
+        },
+        '&:hover fieldset': {
+          borderColor: '#111 !important',
+        },
+        '&.Mui-focused fieldset': {
+          borderColor: '#111 !important',
+          borderWidth: '1px !important',
+        },
+        '&.Mui-error fieldset': {
+          borderColor: '#f44336 !important',
+        },
+      },
+    },
+    MuiPagination: {
+      root: {
+        '& .MuiPagination-ul': {
+          '& .MuiPaginationItem-page': {
+            color: '#333',
+            '&:hover': {
+              backgroundColor: 'rgba(222, 103, 32, 0.25)',
+            },
+          },
+          '& .MuiPaginationItem-page.Mui-selected': {
+            backgroundColor: 'rgba(222, 103, 32, 0.65)',
+          },
+        },
+      },
+    },
+    MuiTooltip: {
+      tooltip: {
+        fontSize: '16px',
+        color: '#fefefe',
+        backgroundColor: '#4d4d4d',
+        borderColor: '#fefefe',
+        borderStyle: 'solid',
+        borderWidth: '1px',
+        borderRadius: 0,
+      },
+      arrow: {
+        '&::before': {
+          backgroundColor: '#4d4d4d',
+        },
+      },
+    },
+  },
+})
+theme = responsiveFontSizes(theme)
+
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+    </ThemeProvider>
   )
 }
+
 
 export default App
