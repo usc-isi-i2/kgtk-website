d8874ddbd19d7640be8180d614009515600ffbb3
g1eb <mail@g1eb.com>
2022-11-08 15:36:56 -0800

    Add a default header/ application bar to the content component

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
+import Header from './Header'
+
 
 const Content = ({children}) => {
 
   return (
     <React.Fragment>
+      <Header />
       {children}
     </React.Fragment>
   )
---
+++ src/components/Header.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import MenuIcon from '@mui/icons-material/Menu'
+
+
+const Header = () => {
+
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" >
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }} >
+            <MenuIcon />
+          </IconButton>
+
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            KGTK - Knowledge Graph ToolKit
+          </Typography>
+
+          <IconButton
+            size="large"
+            edge="end"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+              href="https://github.com/usc-isi-i2/kgtk"
+              title="Knowledge Graph ToolKit"
+              rel="noopener noreferrer nofollow"
+              target="_blank">
+              <GitHubIcon fontSize="large" />
+          </IconButton>
+
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
+
+
+export default Header
