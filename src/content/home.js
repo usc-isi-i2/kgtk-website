706b0adb4952ac0c65703d19ff0818f726735b94
g1eb <mail@g1eb.com>
2022-12-05 11:09:17 -0800

    Add some dummy content for the home page of the website

---
+++ src/content/home.js
@@ -0,0 +1,19 @@
+export const links = [{
+  name: 'So you like tutorials, huh',
+  link: '/tutorials',
+}, {
+  name: 'How to get started',
+  link: '/getting-started',
+}, {
+  name: 'Available data etc',
+  link: '/data',
+}, {
+  name: 'Some more Resources',
+  link: '/resources',
+}, {
+  name: 'Awesome projects powered by KGTK',
+  link: '/use-cases',
+}, {
+  name: 'Who are the folks behind KGTK',
+  link: '/about',
+}]
