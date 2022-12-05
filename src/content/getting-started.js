4ee46c40995edb8d7c83d7b7559537c0d7061e64
g1eb <mail@g1eb.com>
2022-12-05 11:37:14 -0800

    Add more dummy content for the getting started and the tutorials views

---
+++ src/content/getting-started.js
@@ -0,0 +1,28 @@
+export const links = [{
+  name: 'KGTK -- READ THE DOCS',
+  link: 'https://kgtk.readthedocs.io/en/latest/',
+}, {
+  name: 'KGTK -- READ THE DOCS',
+  link: 'https://kgtk.readthedocs.io/en/latest/',
+}, {
+  name: 'KGTK -- READ THE DOCS',
+  link: 'https://kgtk.readthedocs.io/en/latest/',
+}, {
+  name: 'KGTK -- READ THE DOCS',
+  link: 'https://kgtk.readthedocs.io/en/latest/',
+}, {
+  name: 'KGTK -- READ THE DOCS',
+  link: 'https://kgtk.readthedocs.io/en/latest/',
+}, {
+  name: 'KGTK -- READ THE DOCS',
+  link: 'https://kgtk.readthedocs.io/en/latest/',
+}, {
+  name: 'KGTK -- READ THE DOCS',
+  link: 'https://kgtk.readthedocs.io/en/latest/',
+}, {
+  name: 'KGTK -- READ THE DOCS',
+  link: 'https://kgtk.readthedocs.io/en/latest/',
+}, {
+  name: 'KGTK -- READ THE DOCS',
+  link: 'https://kgtk.readthedocs.io/en/latest/',
+}]
---
+++ src/content/tutorials.js
@@ -0,0 +1,7 @@
+export const links = [{
+  name: 'Check out this place where we keep our tutorials',
+  link: 'https://github.com/usc-isi-i2/kgtk/tree/master/tutorial',
+}, {
+  name: 'Here are some jupyter notebooks you can try yourself!',
+  link: 'https://github.com/usc-isi-i2/kgtk-notebooks',
+}]
