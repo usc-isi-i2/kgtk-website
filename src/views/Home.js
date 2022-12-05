2c5f66d6733fa99ff696adb3ceaa786fd28c8720
g1eb <mail@g1eb.com>
2022-12-05 11:38:19 -0800

    Adding dummy content as links to the views/ pages

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import Link from '@mui/material/Link'
 
 import Content from '../components/Content'
+import { links } from '../content/home'
 
 
 const Home = () => {
   return (
     <Content>
-      KGTK
+      <h1>Home</h1>
+      {links.map(link => (
+        <Link href={link.link} underline="hover">
+          <h4>{link.name}</h4>
+        </Link>
+      ))}
     </Content>
   )
 }
--- src/views/Tutorials.js
+++ src/views/Tutorials.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import Link from '@mui/material/Link'
 
 import Content from '../components/Content'
+import { links } from '../content/tutorials'
 
 
 const Tutorials = () => {
   return (
     <Content>
-      Tutorials
+      <h1>Tutorials</h1>
+      {links.map(link => (
+        <Link href={link.link} underline="hover">
+          <h4>{link.name}</h4>
+        </Link>
+      ))}
     </Content>
   )
 }
