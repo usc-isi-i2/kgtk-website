287e334bf87ad0402c16ad2ff25fb21dff89e032
Filip Ilievski <6437407+filievski@users.noreply.github.com>
2022-12-09 09:05:29 -0800

    Update tutorials.js

--- src/content/tutorials.js
+++ src/content/tutorials.js
@@ -5,6 +5,9 @@ export const links = [{
   name: '02. KG Profiling - The goal of profiling is to produce a summary of the classes, properties and instances present in a KG. This notebook shows how to use KGTK to profile a KG, and how KGTK addresses the computational challenges of computing profiles. ',
   url: 'https://colab.research.google.com/drive/1jwq_vVWWwiEa8izA0UZZ_CMK1qIMxBQh?usp=sharing'
 },{
+  name: '03. KG Embeddings - This notebook computes graph embeddings with KGTK and uses them for link prediction.',
+  url: 'https://colab.research.google.com/drive/1Fl4_5LLEtapOxtVlo57hwpPzEK1gFViL?usp=sharing'
+}, {
   name: 'Check out this place where we keep our tutorials',
   url: 'https://github.com/usc-isi-i2/kgtk/tree/master/tutorial',
 }, {
