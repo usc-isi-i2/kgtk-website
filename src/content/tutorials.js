62eb21bdf022ef8ff4b19030d95957ca95d1295d
Filip Ilievski <6437407+filievski@users.noreply.github.com>
2022-12-09 09:03:56 -0800

    Update tutorials.js

--- src/content/tutorials.js
+++ src/content/tutorials.js
@@ -1,4 +1,7 @@
 export const links = [{
+  name: '01. Query knowledge graphs with KGTK Kypher - This tutorial gives an overview of the core features of the KGTK Kypher query language.',
+  url: 'https://colab.research.google.com/drive/1dQeJSYQwpNdIvC9H00SQ90usiUgu3o1E?usp=sharing'
+}, {
   name: '02. KG Profiling - The goal of profiling is to produce a summary of the classes, properties and instances present in a KG. This notebook shows how to use KGTK to profile a KG, and how KGTK addresses the computational challenges of computing profiles. ',
   url: 'https://colab.research.google.com/drive/1jwq_vVWWwiEa8izA0UZZ_CMK1qIMxBQh?usp=sharing'
 },{
