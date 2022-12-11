a6263f6dff08fd712d6506c5bc7b63fd147d523a
g1eb <mail@g1eb.com>
2022-12-10 18:33:44 -0800

    Add project description to the banner image on the home page

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -27,10 +27,26 @@ const Title = styled('h1')(`
   display: inline-block;
   width: 100%;
   padding: 0 5vw;
+  margin: 2.5rem 0 0.5rem 0;
   text-align: right;
   font-family: "Montserrat";
   font-size: 7rem;
-  color: #333;
+  color: #fefefe;
+  pointer-events: none;
+  user-select: none;
+  z-index: 1;
+`)
+
+
+const Description = styled('h4')(`
+  display: inline-block;
+  width: 100%;
+  padding: 0 5vw;
+  margin: 1rem 0;
+  text-align: right;
+  font-family: "Montserrat";
+  font-size: 7rem;
+  color: #fefefe;
   pointer-events: none;
   user-select: none;
   z-index: 1;
@@ -44,7 +60,12 @@ const Banner = () => {
   }
 
   const renderProjectInfo = () => {
-    return <Title>{info.title}</Title>
+    return (
+      <React.Fragment>
+        <Title>{info.title}</Title>
+        <Description>{info.description}</Description>
+      </React.Fragment>
+    )
   }
 
   return (
