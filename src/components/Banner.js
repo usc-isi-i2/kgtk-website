6f832e9888a43191c2831d2f557fffe636ffd445
g1eb <mail@g1eb.com>
2022-12-11 08:35:35 -0800

    Move our graph animation over to its own dedicated component file

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,14 +1,12 @@
-import React, { useCallback } from 'react'
+import React from 'react'
 
 import { styled } from '@mui/material/styles'
-
-import Particles from 'react-particles'
-import { loadFull } from 'tsparticles'
 
 import '@fontsource/ubuntu/700.css'
 
 import { info } from '../content/home'
 import bannerImage from '../images/banner.jpg'
+import GraphAnimation from './GraphAnimation'
 
 
 const BannerImage = styled('div')(`
@@ -95,110 +93,12 @@ const Description = styled('text')(`
 
 const Banner = () => {
 
-  const particlesInit = useCallback(async engine => {
-    await loadFull(engine)
-  }, [])
-
-  const particlesLoaded = useCallback(async container => {
-    await console.log(container)
-  }, [])
-
   const renderBannerImage = () => {
-    return  <BannerImage />
+    return <BannerImage />
   }
 
-  const renderParticles = () => {
-    return (
-      <Particles
-        id="tsparticles"
-        init={particlesInit}
-        loaded={particlesLoaded}
-        options={{
-          fpsLimit: 120,
-          interactivity: {
-            events: {
-              onClick: {
-                enable: true,
-                mode: "push",
-              },
-              onHover: {
-                enable: true,
-                mode: "repulse",
-              },
-              resize: true,
-            },
-            modes: {
-              push: {
-                quantity: 1,
-              },
-              repulse: {
-                distance: 1,
-                duration: 1,
-              },
-            },
-          },
-          particles: {
-            color: {
-              value: '#fefefe',
-            },
-            links: {
-              color: '#fefefe',
-              distance: 150,
-              enable: true,
-              opacity: 0.25,
-              width: 1,
-            },
-            collisions: {
-              enable: true,
-            },
-            move: {
-              directions: 'none',
-              enable: true,
-              outModes: {
-                default: 'bounce',
-              },
-              random: false,
-              speed: 1,
-              straight: false,
-            },
-            interactivity: {
-              detectsOn: "canvas",
-              events: {
-                onHover: {
-                  enable: true,
-                  mode: "attract"
-                },
-                onClick: {
-                  enable: true,
-                  mode: "attract"
-                },
-                resize: true
-              },
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 1000,
-              },
-              value: 100,
-            },
-            opacity: {
-              value: 0.25,
-            },
-            shape: {
-              type: 'circle',
-            },
-            size: {
-              value: {
-                min: 1,
-                max: 5,
-              },
-            },
-          },
-          detectRetina: true,
-        }}
-      />
-    )
+  const renderAnimation = () => {
+    return <GraphAnimation />
   }
 
   const renderTitle = () => {
@@ -219,7 +119,7 @@ const Banner = () => {
   return (
     <React.Fragment>
       {renderBannerImage()}
-      {renderParticles()}
+      {renderAnimation()}
       {renderTitle()}
     </React.Fragment>
   )
---
+++ src/components/GraphAnimation.js
@@ -0,0 +1,119 @@
+import React, { useCallback } from 'react'
+
+import Particles from 'react-particles'
+import { loadFull } from 'tsparticles'
+
+
+const GraphAnimation = () => {
+
+  const particlesInit = useCallback(async engine => {
+    await loadFull(engine)
+  }, [])
+
+  const particlesLoaded = useCallback(async container => {
+    await console.log(container)
+  }, [])
+
+  const renderParticles = () => {
+    return (
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: 'push',
+              },
+              onHover: {
+                enable: true,
+                mode: 'repulse',
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 1,
+              },
+              repulse: {
+                distance: 1,
+                duration: 1,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: '#fefefe',
+            },
+            links: {
+              color: '#fefefe',
+              distance: 150,
+              enable: true,
+              opacity: 0.25,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              directions: 'none',
+              enable: true,
+              outModes: {
+                default: 'bounce',
+              },
+              random: false,
+              speed: 1,
+              straight: false,
+            },
+            interactivity: {
+              detectsOn: 'canvas',
+              events: {
+                onHover: {
+                  enable: true,
+                  mode: 'attract'
+                },
+                onClick: {
+                  enable: true,
+                  mode: 'attract'
+                },
+                resize: true
+              },
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 1000,
+              },
+              value: 100,
+            },
+            opacity: {
+              value: 0.25,
+            },
+            shape: {
+              type: 'circle',
+            },
+            size: {
+              value: {
+                min: 1,
+                max: 5,
+              },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+    )
+  }
+
+  return (
+    <React.Fragment>
+      {renderParticles()}
+    </React.Fragment>
+  )
+}
+
+
+export default GraphAnimation
