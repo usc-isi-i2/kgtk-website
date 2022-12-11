c0d3ac1c82f7fafd9b6f3dc9355de63a540d4408
g1eb <mail@g1eb.com>
2022-12-11 08:25:20 -0800

    Add particles animation to the background on the home page

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 
 import { styled } from '@mui/material/styles'
+
+import Particles from 'react-particles'
+import { loadFull } from 'tsparticles'
 
 import '@fontsource/ubuntu/700.css'
 
@@ -92,8 +95,110 @@ const Description = styled('text')(`
 
 const Banner = () => {
 
+  const particlesInit = useCallback(async engine => {
+    await loadFull(engine)
+  }, [])
+
+  const particlesLoaded = useCallback(async container => {
+    await console.log(container)
+  }, [])
+
   const renderBannerImage = () => {
     return  <BannerImage />
+  }
+
+  const renderParticles = () => {
+    return (
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 1,
+              },
+              repulse: {
+                distance: 1,
+                duration: 1,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: '#fefefe',
+            },
+            links: {
+              color: '#fefefe',
+              distance: 150,
+              enable: true,
+              opacity: 0.25,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              directions: 'none',
+              enable: true,
+              outModes: {
+                default: 'bounce',
+              },
+              random: false,
+              speed: 1,
+              straight: false,
+            },
+            interactivity: {
+              detectsOn: "canvas",
+              events: {
+                onHover: {
+                  enable: true,
+                  mode: "attract"
+                },
+                onClick: {
+                  enable: true,
+                  mode: "attract"
+                },
+                resize: true
+              },
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 1000,
+              },
+              value: 100,
+            },
+            opacity: {
+              value: 0.25,
+            },
+            shape: {
+              type: 'circle',
+            },
+            size: {
+              value: {
+                min: 1,
+                max: 5,
+              },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+    )
   }
 
   const renderTitle = () => {
@@ -114,6 +219,7 @@ const Banner = () => {
   return (
     <React.Fragment>
       {renderBannerImage()}
+      {renderParticles()}
       {renderTitle()}
     </React.Fragment>
   )
