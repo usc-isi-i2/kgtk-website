c43cc65f4b0f92c8fa205d200ec9b3ff8cc9b2a5
g1eb <mail@g1eb.com>
2022-12-10 17:46:20 -0800

    Add css styling to make sure the banner image is always in the background

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,29 @@
 import React from 'react'
 
+import { styled } from '@mui/material/styles'
+
+import bannerImage from '../images/banner.jpg'
+
+
+const BannerImage = styled('img')(`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  opacity: 1;
+  transition: opacity 500ms ease-in 0s;
+  pointer-events: none;
+  user-select: none;
+  z-index: -1;
+`)
+
 
 const Banner = () => {
 
   return (
     <React.Fragment>
+      <BannerImage src={bannerImage} />
     </React.Fragment>
   )
 }
