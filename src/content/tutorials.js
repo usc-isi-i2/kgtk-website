a6357fab940692b9ce9ff339af4af94a851cd8cb
g1eb <mail@g1eb.com>
2023-02-06 06:26:54 -0800

    Update content on the tutorials tab to only include AAAI 2023 for now

--- src/content/tutorials.js
+++ src/content/tutorials.js
@@ -1,16 +1,4 @@
 export const links = [{
-  name: '01. Query knowledge graphs with KGTK Kypher - This tutorial gives an overview of the core features of the KGTK Kypher query language.',
-  url: 'https://colab.research.google.com/drive/1dQeJSYQwpNdIvC9H00SQ90usiUgu3o1E?usp=sharing'
-}, {
-  name: '02. KG Profiling - The goal of profiling is to produce a summary of the classes, properties and instances present in a KG. This notebook shows how to use KGTK to profile a KG, and how KGTK addresses the computational challenges of computing profiles. ',
-  url: 'https://colab.research.google.com/drive/1jwq_vVWWwiEa8izA0UZZ_CMK1qIMxBQh?usp=sharing'
-},{
-  name: '03. KG Embeddings - This notebook computes graph embeddings with KGTK and uses them for link prediction.',
-  url: 'https://colab.research.google.com/drive/1Fl4_5LLEtapOxtVlo57hwpPzEK1gFViL?usp=sharing'
-}, {
-  name: 'Check out this place where we keep our tutorials',
-  url: 'https://github.com/usc-isi-i2/kgtk/tree/master/tutorial',
-}, {
-  name: 'Here are some jupyter notebooks you can try yourself!',
-  url: 'https://github.com/usc-isi-i2/kgtk-notebooks',
+  name: 'KGTK: User-friendly Toolkit for Manipulation of Large Knowledge Graphs',
+  url: 'https://usc-isi-i2.github.io/kgtk-tutorial-aaai-2023/',
 }]
