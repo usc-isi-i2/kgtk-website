dd94e15f4855a85ce4211d206c5dd25c8ce31831
g1eb <mail@g1eb.com>
2023-02-10 11:27:19 -0800

    Add all of the kgtk tutorials to the tutorials page

--- src/content/tutorials.js
+++ src/content/tutorials.js
@@ -1,4 +1,25 @@
 export const links = [{
-  name: 'KGTK: User-friendly Toolkit for Manipulation of Large Knowledge Graphs',
+  name: 'AAAI-23 - KGTK: User-friendly Toolkit for Manipulation of Large Knowledge Graphs',
   url: 'https://usc-isi-i2.github.io/kgtk-tutorial-aaai-2023/',
+}, {
+  name: '01-kgtk-introduction',
+  url: 'https://colab.research.google.com/drive/1dQeJSYQwpNdIvC9H00SQ90usiUgu3o1E?usp=sharing',
+}, {
+  name: '02-kg-profiling',
+  url: 'https://colab.research.google.com/drive/1jwq_vVWWwiEa8izA0UZZ_CMK1qIMxBQh?usp=sharing',
+}, {
+  name: '03-kg-graph-embeddings',
+  url: 'https://colab.research.google.com/drive/1Fl4_5LLEtapOxtVlo57hwpPzEK1gFViL?usp=sharing',
+}, {
+  name: '04-kg-enrichment-with-csv',
+  url: 'https://colab.research.google.com/drive/1tqKvFTFFQoBCA9GZ5WZPVJhJq5fBem49?usp=sharing',
+}, {
+  name: '05-kg-enrichment-with-lod',
+  url: 'https://colab.research.google.com/drive/1vXttxSVtJeSDPlCrpL88G6TGhXdoC3ve?usp=sharing',
+}, {
+  name: '06-kg-network-analysis',
+  url: 'https://colab.research.google.com/drive/1Lat732XpHv1RMswoYsz_wUk_6eKEf8Xt?usp=sharing',
+}, {
+  name: '07-kg-constraint-validation',
+  url: 'https://colab.research.google.com/drive/182ikHCeGDhyPs8WuLQC03c-ET84p0ozM?usp=sharing',
 }]
