91c128fa302eeb6de45c7e33ad9cef136f72f833
g1eb <mail@g1eb.com>
2023-02-12 09:45:20 -0800

    Use cards to render use-cases on the page

--- src/views/UseCases.js
+++ src/views/UseCases.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import Box from '@mui/material/Box'
 import Link from '@mui/material/Link'
+import Card from '@mui/material/Card'
+import CardMedia from '@mui/material/CardMedia'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
 
 import Content from '../components/Content'
 import { links } from '../content/use-cases'
@@ -10,9 +15,28 @@ const UseCases = () => {
     <Content>
       <h1>Use Cases</h1>
       {links.map(link => (
-        <Link href={link.url} underline="hover" target="_blank" rel="noopener noreferrer">
-          <h4>{link.name}</h4>
-        </Link>
+        <Card key={link.name}>
+          <Box sx={{
+            display: 'flex',
+            flexDirection: 'row'
+          }}>
+            <CardMedia
+              sx={{ height: 200 }}
+              component="img"
+              image={link.image}
+              title={link.name} />
+            <CardContent>
+              <Link href={link.url} underline="hover" target="_blank" rel="noopener noreferrer">
+                <Typography component="h5" variant="h5">
+                  <b>{link.name}</b>
+                </Typography>
+              </Link>
+              <Typography component="p" variant="body1">
+                {link.description}
+              </Typography>
+            </CardContent>
+          </Box>
+        </Card>
       ))}
     </Content>
   )
