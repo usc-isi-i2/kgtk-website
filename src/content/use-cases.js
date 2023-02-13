d0ba832b8ee73b38e86b79b9bd575769e5a433b2
Filip Ilievski <6437407+filievski@users.noreply.github.com>
2023-02-13 14:08:44 -0800

    Update use-cases.js

--- src/content/use-cases.js
+++ src/content/use-cases.js
@@ -1,17 +1,17 @@
 export const links = [{
   name: 'Internet Memes: Knowledge connects culture and creativity',
   description: 'KGTK can help connect the dots between internet meme sources and external knowledge graphs, like Wikidata. We will use KGTK to perform scalable analytics of the resulting graph and execute novel entity-centric and hybrid queries.',
-  image: 'https://raw.githubusercontent.com/usc-isi-i2/kgtk-website/develop/src/images/tutorials/temp.jpg',
+  image: 'https://raw.githubusercontent.com/usc-isi-i2/kgtk-website/develop/src/images/tutorials/memes.png',
 }, {
   name: 'Financial transactions: Detecting anomalies in trading',
   description: 'KGTK can be used to analyze financial transaction data. We will illustrate how to construct KGTK pipelines with graph transformations, analytics and visualization steps for the financial sector. The KGTK pipelines enable us to highlight trading behaviors, to find potential colluders, and to find inconsistencies through differentiating knowledge graph structures.',
-  image: 'https://raw.githubusercontent.com/usc-isi-i2/kgtk-website/develop/src/images/tutorials/temp.jpg',
+  image: 'https://raw.githubusercontent.com/usc-isi-i2/kgtk-website/develop/src/images/tutorials/finsec.png',
 }, {
   name: 'Publication Graphs (PubGraphs): What should I read next?',
   description: 'The recent advent of public large-scale research publications metadata repositories such as OpenAlex (Priem, Piwowar, and Orr 2022) enables us to study innovation at scales that have not been possible before. However, dealing with these large-scale repositories is extremely difficult and requires special toolkits. In this session, we will describe how KGTK can be used for data filtering, data transformation, knowledge graph extraction, and knowledge graph embedding training of knowledge graphs with scientific publications.',
-  image: 'https://raw.githubusercontent.com/usc-isi-i2/kgtk-website/develop/src/images/tutorials/temp.jpg',
+  image: 'https://raw.githubusercontent.com/usc-isi-i2/kgtk-website/develop/src/images/tutorials/pubgraph.png',
 }, {
   name: 'Morality in events: From news to timelines and graph maps',
   description: 'Our knowledge graph tools can be applied to make sense of complex events. Focused on a specific domain (or location) we track the changes in moral foundations (Johnson and Goldwasser 2018) and emotions to understand public perception of these events. The use of KGTK in this project makes it easy to scale up, to generalize to other domains and locations, and to browse and visualize the data.',
-  image: 'https://raw.githubusercontent.com/usc-isi-i2/kgtk-website/develop/src/images/tutorials/temp.jpg',
+  image: 'https://raw.githubusercontent.com/usc-isi-i2/kgtk-website/develop/src/images/tutorials/venice.png',
 }]
