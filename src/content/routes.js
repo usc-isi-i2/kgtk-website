c79cc96d31787400bd2ad87501943edcdcf38afd
g1eb <mail@g1eb.com>
2023-02-24 12:13:22 -0800

    Add the initial publications view

--- src/content/routes.js
+++ src/content/routes.js
@@ -2,6 +2,7 @@ import Home from '../views/Home'
 import Tutorials from '../views/Tutorials'
 import Data from '../views/Data'
 import Resources from '../views/Resources'
+import Publications from '../views/Publications'
 import UseCases from '../views/UseCases'
 import About from '../views/About'
 
@@ -22,6 +23,10 @@ const routes = [{
   name: 'Resources',
   path: '/resources',
   element: <Resources />,
+}, {
+  name: 'Publications',
+  path: '/publications',
+  element: <Publications />,
 }, {
   name: 'Use Cases',
   path: '/use-cases',
---
+++ src/views/Publications.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from '@mui/material/Link'
+
+import Content from '../components/Content'
+
+
+const Publications = () => {
+  return (
+    <Content>
+      <h1>Publications</h1>
+      <hr />
+    </Content>
+  )
+}
+
+
+export default Publications
