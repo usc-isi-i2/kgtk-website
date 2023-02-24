0c3017492776ca17a9b270e18a079c420434d901
g1eb <mail@g1eb.com>
2023-02-24 12:42:58 -0800

    Read publications in from a referenecs.bib file and parse them with bibtex lib

--- src/views/Publications.js
+++ src/views/Publications.js
@@ -1,14 +1,37 @@
-import React from 'react'
-import Link from '@mui/material/Link'
+import React, { useEffect, useState } from 'react'
+
+import { parseBibFile, normalizeFieldValue } from 'bibtex'
 
 import Content from '../components/Content'
+import references from '../content/references.bib'
 
 
 const Publications = () => {
+
+  const [publications, setPublications] = useState()
+
+  useEffect(() => {
+
+    fetch(references)
+      .then(refs => refs.text())
+      .then(text => {
+        setPublications(
+          parseBibFile(text)
+        )
+      })
+
+  }, [])
+
+  const renderPublications = () => {
+    if ( !publications ) { return }
+    return Object.keys(publications.entries$).map(key => <h1>{key}</h1>)
+  }
+
   return (
     <Content>
       <h1>Publications</h1>
       <hr />
+      {renderPublications()}
     </Content>
   )
 }
