5f36a8cdc74521e4839b10f89deb90567f62c830
g1eb <mail@g1eb.com>
2023-02-24 10:53:01 -0800

    Add a subtitle to the data view

--- src/views/Data.js
+++ src/views/Data.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from '@mui/material/Link'
+import Typography from '@mui/material/Typography'
 
 import Content from '../components/Content'
 import { links } from '../content/data.js'
@@ -8,7 +9,11 @@ import { links } from '../content/data.js'
 const Data = () => {
   return (
     <Content>
-      <h1>Available datasets:</h1>
+      <h1>Data Catalog</h1>
+      <hr />
+      <Typography component="h5" variant="h5">
+        Wikidata 11-2022
+      </Typography>
       {links.map(link => (
         <Link href={link.url} underline="hover" target="_blank" rel="noopener noreferrer">
           <h4>{link.name}</h4>
