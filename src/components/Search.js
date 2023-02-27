3b48bbf5b67b9617ada17de213df769271271f2c
g1eb <mail@g1eb.com>
2023-02-27 12:30:42 -0800

    Add a search icon to the publications search input field

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import TextField from '@mui/material/TextField'
+import InputAdornment from '@mui/material/InputAdornment'
+import SearchIcon from '@mui/icons-material/Search'
 import { styled } from '@mui/material/styles'
 
 
@@ -12,6 +14,7 @@ const Search = ({text, label, updateText}) => {
 
   return (
     <TextField
+      fullWidth
       name={text}
       label={label}
       value={text}
@@ -20,7 +23,13 @@ const Search = ({text, label, updateText}) => {
       autoComplete="off"
       onChange={handleOnChange}
       variant="standard"
-      fullWidth
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <SearchIcon />
+          </InputAdornment>
+        ),
+      }}
     />
   )
 }
