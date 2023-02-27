fc256e172a4407af52e8df8b37da41bc654eddc4
g1eb <mail@g1eb.com>
2023-02-27 15:24:15 -0800

    Link to the pages associated with folks on the about page

--- src/views/About.js
+++ src/views/About.js
@@ -20,27 +20,32 @@ const About = () => {
         </Typography>
         <br />
         {links.filter(link => link.role === role).map(link => (
-          <Card key={link.name} sx={{
-            display: 'inline-block',
-            marginRight: '2em',
-            width: 250,
-          }}>
-            <Box sx={{
-              display: 'flex',
-              flexDirection: 'column',
+          <Link key={link.name}
+            href={link.url}
+            target="_blank"
+            rel="noopener noreferrer">
+            <Card sx={{
+              display: 'inline-block',
+              marginRight: '2em',
+              width: 250,
             }}>
-              <CardMedia
-                sx={{ height: 300 }}
-                component="img"
-                image={link.image}
-                title={link.name} />
-              <CardContent>
-                <Typography component="h5" variant="h5" textAlign="center">
-                  <b>{link.name}</b>
-                </Typography>
-              </CardContent>
-            </Box>
-          </Card>
+              <Box sx={{
+                display: 'flex',
+                flexDirection: 'column',
+              }}>
+                <CardMedia
+                  sx={{ height: 300 }}
+                  component="img"
+                  image={link.image}
+                  title={link.name} />
+                <CardContent>
+                  <Typography component="h5" variant="h5" textAlign="center">
+                    <b>{link.name}</b>
+                  </Typography>
+                </CardContent>
+              </Box>
+            </Card>
+          </Link>
         ))}
       </Box>
     )
