3209dd79039e9937272d22c12169055c3eedbe18
g1eb <mail@g1eb.com>
2023-02-27 14:49:19 -0800

    Change layout of the about page to show cards with pictures instead

--- src/views/About.js
+++ src/views/About.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import CardMedia from '@mui/material/CardMedia'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
 import Link from '@mui/material/Link'
 
 import Content from '../components/Content'
@@ -11,9 +16,27 @@ const About = () => {
       <h1>The KGTK Team</h1>
       <hr />
       {links.map(link => (
-        <Link href={link.url} underline="hover" target="_blank" rel="noopener noreferrer">
-          <h4>{link.name}</h4>
-        </Link>
+        <Card key={link.name} sx={{
+          display: 'inline-block',
+          marginRight: '2em',
+          width: 250,
+        }}>
+          <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+          }}>
+            <CardMedia
+              sx={{ height: 300 }}
+              component="img"
+              image={'http://placekitten.com/200/300'}
+              title={link.name} />
+            <CardContent>
+              <Typography component="h5" variant="h5" textAlign="center">
+                <b>{link.name}</b>
+              </Typography>
+            </CardContent>
+          </Box>
+        </Card>
       ))}
     </Content>
   )
