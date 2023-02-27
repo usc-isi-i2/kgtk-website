87dae29b2b4af9ab8c6dc1b498339d2b1060a00f
g1eb <mail@g1eb.com>
2023-02-27 14:47:12 -0800

    Update folks listed on the about page, adding roles

--- src/content/about.js
+++ src/content/about.js
@@ -1,19 +1,37 @@
 export const links = [{
   name: 'Filip Ilievski',
+  role: 'Researcher',
   url: 'https://www.linkedin.com/in/filipilievski91/',
 }, {
   name: 'Hans Chalupsky',
+  role: 'Researcher',
   url: 'https://www.linkedin.com/in/hans-chalupsky-a3416b3/',
 }, {
   name: 'Pedro Szekely',
+  role: 'Researcher',
   url: 'https://www.linkedin.com/in/szeke/',
 }, {
   name: 'Jay Pujara',
+  role: 'Researcher',
   url: 'https://www.linkedin.com/in/pujara/',
 }, {
+  name: 'Daniel Schwabe',
+  role: 'Collaborator',
+  url: 'https://www.linkedin.com/in/pujara/',
+}, {
+  name: 'Daniel Garijo',
+  role: 'Collaborator',
+  url: 'https://www.linkedin.com/in/pujara/',
+}, {
+  name: 'Craig Rogers',
+  role: 'Engineer',
+  url: 'https://www.linkedin.com/in/amandeepsaggu/',
+}, {
   name: 'Amandeep Singh',
+  role: 'Engineer',
   url: 'https://www.linkedin.com/in/amandeepsaggu/',
 }, {
   name: 'Gleb Satyukov',
+  role: 'Engineer',
   url: 'https://www.linkedin.com/in/g1eb-/',
 }]
