0f6a35374c136db5e89c1b7a6d2c9b4c3d26860f
g1eb <mail@g1eb.com>
2023-02-27 14:55:14 -0800

    Render folks filtered by role on the about page

--- src/views/About.js
+++ src/views/About.js
@@ -11,33 +11,48 @@ import { links } from '../content/about'
 
 
 const About = () => {
+
+  const renderByRole = role => {
+    return (
+      <Box>
+        <Typography component="h4" variant="h4">
+          {role}s
+        </Typography>
+        <br />
+        {links.filter(link => link.role === role).map(link => (
+          <Card key={link.name} sx={{
+            display: 'inline-block',
+            marginRight: '2em',
+            width: 250,
+          }}>
+            <Box sx={{
+              display: 'flex',
+              flexDirection: 'column',
+            }}>
+              <CardMedia
+                sx={{ height: 300 }}
+                component="img"
+                image={'http://placekitten.com/200/300'}
+                title={link.name} />
+              <CardContent>
+                <Typography component="h5" variant="h5" textAlign="center">
+                  <b>{link.name}</b>
+                </Typography>
+              </CardContent>
+            </Box>
+          </Card>
+        ))}
+      </Box>
+    )
+  }
+
   return (
     <Content>
       <h1>The KGTK Team</h1>
       <hr />
-      {links.map(link => (
-        <Card key={link.name} sx={{
-          display: 'inline-block',
-          marginRight: '2em',
-          width: 250,
-        }}>
-          <Box sx={{
-            display: 'flex',
-            flexDirection: 'column',
-          }}>
-            <CardMedia
-              sx={{ height: 300 }}
-              component="img"
-              image={'http://placekitten.com/200/300'}
-              title={link.name} />
-            <CardContent>
-              <Typography component="h5" variant="h5" textAlign="center">
-                <b>{link.name}</b>
-              </Typography>
-            </CardContent>
-          </Box>
-        </Card>
-      ))}
+      {renderByRole('Researcher')}
+      {renderByRole('Collaborator')}
+      {renderByRole('Engineer')}
     </Content>
   )
 }
